98d839c793f165bf5ae44e390da88130f36c9628
Filip <43039909+Urfisior@users.noreply.github.com>
2020-07-08 22:34:43 +0100

    all animation done

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 class AppCovid {
-    constructor() {
-        this.firstAppBackground = document.querySelector('.start');
-        this.frontBtn = document.querySelector('.button');
-        this.hiddenFirstBackground()
-    }
-    hiddenFirstBackground() {
-        this.frontBtn.addEventListener('click', () => {
-            this.firstAppBackground.style.left = '-100%';
-        })
-    }
+  constructor() {
+    this.firstAppBackground = document.querySelector(".start");
+    this.frontBtn = document.querySelector(".button");
+    this.h1 = document.querySelector(".search__main-text");
+    this.searchBtn = document.querySelector(".search__button");
+    this.input = document.querySelector(".search__input");
+    this.countryConteiner = document.querySelector(".country");
+    this.hiddenFirstBackground();
+  }
+  hiddenFirstBackground() {
+    this.frontBtn.addEventListener("click", () => {
+      this.firstAppBackground.style.left = "-100%";
+      this.h1.classList.add("search__main-text--animation");
+      this.searchBtn.classList.add("search__button--aply");
+      this.input.classList.add("search__input--animation");
+      this.countryConteiner.classList.add("country--animation");
+    });
+  }
 }
 
-new AppCovid()
+new AppCovid();
